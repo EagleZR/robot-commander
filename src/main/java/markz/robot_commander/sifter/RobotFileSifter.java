ca14ced4e04a8c6961efc855b6a0647784df98dc
Mark Zeagler <mark.zeagler@outlook.com>
2019-01-26 20:08:48 -0500

    Added Copyright Notices

--- src/main/java/markz/robot_commander/sifter/RobotFileSifter.java
+++ src/main/java/markz/robot_commander/sifter/RobotFileSifter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019. Mark Zeagler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package markz.robot_commander.sifter;
 
 import java.io.File;
@@ -27,8 +43,7 @@ public class RobotFileSifter {
 				returnList.addAll( getRobotFiles( child ) );
 			} else {
 				for ( String fileExtension : fileExtensions ) {
-					if ( child.getName().endsWith( fileExtension ) && !IGNORED_FILE_NAMES
-							.contains( child.getName() ) ) {
+					if ( child.getName().endsWith( fileExtension ) && !IGNORED_FILE_NAMES.contains( child.getName() ) ) {
 						returnList.add( child );
 					}
 				}
